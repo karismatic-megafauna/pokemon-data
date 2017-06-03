8dcba4a5346645a70f786fb9272f987b48cf0f10
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-06-02 17:00:55 -0700

    Add rate limiter

--- pokeScraper.js
+++ pokeScraper.js
@@ -1,21 +1,51 @@
-var fetch = require('~/.nvm/versions/node/v7.10.0/lib/node_modules/node-fetch');
+var fetch = require('node-fetch');
+var RateLimiter = require('limiter').RateLimiter;
+var fs = require('fs');
+var limiter = new RateLimiter(1, 500);
 
-fetch(`https://pokeapi.co/api/v2/pokemon/1/`)
-  .then((response) => {
-    response.json()
-      .then( (data) => {
-        console.log(data);
-      })
+const getPokemonById = (id) => {
+  return new Promise((resolve, reject) => {
+    limiter.removeTokens(1, () => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then((response) => {
+          response.json().then((data) => {
+            resolve(formatResponse(data));
+          })
+        })
+        .catch((e) => {
+          reject(e)
+        })
+    })
   })
+};
 
+let promiseArray = [];
+for(var i = 1; i <= 4; i++) {
+  promiseArray.push(getPokemonById(i));
+}
 
-const getDataWeWant = (data) => {
-  const subsetOfData = {
-    stats: data.stats,
-    name: data.name,
-    moves: data.moves,
-    height: data.height,
+Promise.all(promiseArray).then(value => {
+  let formattedValue = JSON.stringify(value, null, 2);
+  fs.writeFile('data.json', formattedValue, (err) => {
+    if (err) throw err;
+    console.log('Complete');
+  });
+}, reason => {
+  console.log(reason);
+});
+
+const formatResponse = (data) => {
+  let newObj = {
+    [data.name] : {
+      id: data.id,
+      height: data.height,
+      weight: data.weight,
+      sprites: data.sprites,
+      stats: data.stats,
+      base_experience: data.base_experience,
+      types: data.types,
+    },
   }
 
-  return subsetOfData;
+  return newObj;
 }
